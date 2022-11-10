68d0275e6e0f440e25d32af1b58435ffbca8b8ad
Posh1Gamer <Josh.d.jonathan@gmail.com>
2022-11-10 06:57:26 -0500

    wheel diameter and kP

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,14 +20,14 @@ import edu.wpi.first.wpilibj.XboxController;
 public final class Constants {
 
     public static final class ModuleConstants {
-        public static final double kWheelDiameterMeters = Units.inchesToMeters(3.75); //needs to be changed
+        public static final double kWheelDiameterMeters = Units.inchesToMeters(3.8); //needs to be changed
         public static final double kDriveMotorGearRatio = 6.75; 
         public static final double kTurningMotorGearRatio = 21.4286; //needs to be changed
         public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
         public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
         public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
         public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
-        public static final double kPTurning = 1; //idk
+        public static final double kPTurning = 0.5; //idk
     }
 
     public static final class DriveConstants {
