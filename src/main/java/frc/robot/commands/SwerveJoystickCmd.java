c3ca39955eea8f7f309b5aeb3cc769b92f6ba511
Posh1Gamer <Josh.d.jonathan@gmail.com>
2022-11-18 06:53:29 -0500

    functional scalar deadband

--- src/main/java/frc/robot/commands/SwerveJoystickCmd.java
+++ src/main/java/frc/robot/commands/SwerveJoystickCmd.java
@@ -12,6 +12,7 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.ModuleConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.SwerveSubsystem;
 
@@ -20,7 +21,13 @@ public class SwerveJoystickCmd extends CommandBase {
   private final Supplier<Double> xSpdFunction, ySpdFunction, turningSpdFunction;
   private final Supplier<Boolean> fieldOrientedFunction; //whether user wants command to be field oriented
   private final SlewRateLimiter xLimiter, yLimiter, turningLimiter;
-  
+  private double rawMagnitudeTranslation = 0;
+  private double scaledMagnitudeTranslation = 0;
+  private double directionTranslation[] = {0, 0};
+  private double rawMagnitudeRotation = 0;
+  private double scaledMagnitudeRotation = 0;
+  private double directionRotation = 0;
+
   /** Creates a new SwerveJoystickCmd. */
   public SwerveJoystickCmd(SwerveSubsystem swerveSubsystem,
       Supplier<Double> xSpdFunction, Supplier<Double> ySpdFunction, Supplier<Double> turningSpdFunction,
@@ -50,9 +57,27 @@ public class SwerveJoystickCmd extends CommandBase {
 
     // 2. Apply deadband
     //debug output: SmartDashboard.putNumber("input", xSpeed);
-    xSpeed = Math.sqrt(Math.pow(xSpeed, 2) + Math.pow(ySpeed, 2)) > OIConstants.kDeadband ? xSpeed : 0.0;
-    ySpeed = Math.sqrt(Math.pow(xSpeed, 2) + Math.pow(ySpeed, 2)) > OIConstants.kDeadband ? ySpeed : 0.0;
-    turningSpeed = Math.abs(turningSpeed) > OIConstants.kDeadband ? turningSpeed : 0.0;
+    //raw inputs
+    rawMagnitudeTranslation = Math.sqrt(Math.pow(xSpeed, 2) + Math.pow(ySpeed, 2)); //magnitude of joystick input
+    directionTranslation[0] = xSpeed/rawMagnitudeTranslation; //x component of raw input
+    directionTranslation[1] = ySpeed/rawMagnitudeTranslation;
+    //scaling
+    scaledMagnitudeTranslation = (1/(1-OIConstants.kDeadband))*(rawMagnitudeTranslation-OIConstants.kDeadband); //converted to be representative of deadzone using point slope form (y-y1)=(m)(x-x1) -> (scaled-minimun raw input)=(maximum input/(1-deadzone))(raw input-deadzone) -> scaled=(maximum input/(1-deadzone))(raw input-deadzone)
+    if (rawMagnitudeTranslation > OIConstants.kDeadband) {
+      xSpeed = directionTranslation[0] * scaledMagnitudeTranslation; //original direction, scaled magnitude... this is kinda a misnomer because this x component itself is a magnitude, but it is representative of a direction of the raw input
+      ySpeed = directionTranslation[1] * scaledMagnitudeTranslation;
+    } else {
+      xSpeed = 0.0;
+      ySpeed = 0.0; //zero tiny inputs
+    }
+    //raw inputs
+    rawMagnitudeRotation = Math.abs(turningSpeed); //magnitude of joystick input
+    directionRotation = turningSpeed/rawMagnitudeRotation; //conveys polarity +/-
+    //scaling
+    scaledMagnitudeRotation = (1/(1-OIConstants.kDeadband))*(rawMagnitudeRotation-OIConstants.kDeadband); //same algorithm as scaled magnitude above
+    if (Math.abs(turningSpeed) > OIConstants.kDeadband) {
+      turningSpeed = directionRotation * scaledMagnitudeRotation; //same as above for translation
+    } else turningSpeed = 0.0; //zero tiny inputs
 
     // 3. Make the driving smoother, no sudden acceleration from sudden inputs
     xSpeed = xLimiter.calculate(xSpeed * DriveConstants.kTeleDriveMaxSpeedMetersPerSecond);
@@ -63,7 +88,7 @@ public class SwerveJoystickCmd extends CommandBase {
 
     // 4. Construct desired chassis speeds (convert to appropriate reference frames)
     ChassisSpeeds chassisSpeeds;
-    if (fieldOrientedFunction.get()) {
+    if (/*fieldOrientedFunction.get()*/ true) {
       // Relative to field
       chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
           xSpeed, ySpeed, turningSpeed, swerveSubsystem.getRotation2d());
