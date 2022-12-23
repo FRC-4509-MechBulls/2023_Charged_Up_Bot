725e3c70e46a1cfc0a884bcd55fd9919943d212d
Josh Jonathan <Josh.d.jonathan@gmail.com>
2022-12-22 21:44:32 -0500

    removed disable all telemetry

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -26,7 +26,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    LiveWindow.disableAllTelemetry();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
