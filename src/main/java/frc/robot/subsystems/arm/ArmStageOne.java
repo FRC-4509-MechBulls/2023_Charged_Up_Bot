a655c26b527817d45617499b13cc7268c5a80bc8
Isaac Thoman <isaacthoman@outlook.com>
2023-01-30 08:22:55 -0500

    Stage one uses built in PID?

--- src/main/java/frc/robot/subsystems/arm/ArmStageOne.java
+++ src/main/java/frc/robot/subsystems/arm/ArmStageOne.java
@@ -4,12 +4,14 @@
 
 package frc.robot.subsystems.arm;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 import static frc.robot.Constants.ArmConstants;
 
@@ -19,6 +21,7 @@ public class ArmStageOne extends SubsystemBase {
   private DutyCycleEncoder magEncoder;
   private double encoderOffset = ArmConstants.kStageOne_AbsEncoderInitialOffset;
   private PIDController pid = new PIDController(ArmConstants.stageOne_kP,ArmConstants.stageOne_kI,ArmConstants.stageOne_kD);
+  private double setpointRad = encoderOffset;
 
   /** Creates a new ArmStageOne. */
   public ArmStageOne() {
@@ -31,7 +34,15 @@ public class ArmStageOne extends SubsystemBase {
     armMotorPrimary.follow(armMotorSecondary);
     armMotorPrimary.setInverted(InvertType.OpposeMaster);
 
-    magEncoder = new DutyCycleEncoder(ArmConstants.kStageOne_MagEncoderID);
+    armMotorPrimary.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0,30);
+
+    armMotorPrimary.config_kP(0,ArmConstants.stageOne_kP,30);
+    armMotorPrimary.config_kI(0,ArmConstants.stageOne_kI,30);
+    armMotorPrimary.config_kD(0,ArmConstants.stageOne_kD,30);
+
+    armMotorPrimary.configClosedLoopPeriod(0, 1, 30);
+
+    //magEncoder = new DutyCycleEncoder(ArmConstants.kStageOne_MagEncoderID);
 
     magEncoder.setDistancePerRotation(1);
 
@@ -42,6 +53,13 @@ public class ArmStageOne extends SubsystemBase {
     angle *= Math.PI*2; //convert to radians
     angle += encoderOffset; //add the offset
     return angle * (ArmConstants.kStageOne_AbsEncoderReversed ? -1.0 : 1.0); //multiply -1 if reversed
+  }
+
+  public double getSetpointRaw(){
+    double angle = magEncoder.getAbsolutePosition();
+    angle -= encoderOffset/Math.PI/2;
+    angle += setpointRad/Math.PI/2;
+    return angle;
   }
 
 
@@ -58,6 +76,8 @@ public class ArmStageOne extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    armMotorPrimary.set(TalonSRXControlMode.PercentOutput,pid.calculate(getAbsoluteEncoderRad())); //replace this with internal PID
+    armMotorPrimary.set(TalonSRXControlMode.Position, getSetpointRaw());
+
+    //armMotorPrimary.set(TalonSRXControlMode.PercentOutput,pid.calculate(getAbsoluteEncoderRad())); //replace this with internal PID
   }
 }
