62309081ad0b3f7c1e3818f764a69e4c076b65fe
Isaac Thoman <isaacthoman@outlook.com>
2023-02-15 13:57:02 -0500

    don't set desired pose at init

--- src/main/java/frc/robot/commands/NavToPointCommand.java
+++ src/main/java/frc/robot/commands/NavToPointCommand.java
@@ -23,7 +23,7 @@ public class NavToPointCommand extends CommandBase {
         // each subsystem used by the command must be passed into the
         // addRequirements() method (which takes a vararg of Subsystem)
         addRequirements(this.swerveSubsystem);
-        navigationField.setNavPoint(desiredPose);
+        //navigationField.setNavPoint(desiredPose);
     }
 
     /**
