e1759c2dafad343a2d30bef52f9178ca94340924
Isaac Thoman <isaacthoman@outlook.com>
2023-03-13 11:02:22 -0400

    missing import from merge

--- src/main/java/frc/robot/subsystems/nav/EFNavSystem.java
+++ src/main/java/frc/robot/subsystems/nav/EFNavSystem.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.lib.FieldObjects.FieldLine;
