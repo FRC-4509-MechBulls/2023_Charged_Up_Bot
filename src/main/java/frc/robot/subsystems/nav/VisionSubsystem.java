26dff922cdad9f0213c7d50610b2b9511b8790c0
Isaac Thoman <isaacthoman@outlook.com>
2023-03-21 08:49:47 -0400

    clean out visionSub

--- src/main/java/frc/robot/subsystems/nav/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/nav/VisionSubsystem.java
@@ -29,40 +29,12 @@ public class VisionSubsystem extends SubsystemBase {
     private SwerveSubsystem swerveSubsystem;
     public VisionSubsystem(SwerveSubsystem swerveSubsystem, PathingTelemetrySub pathingTelemetrySub) {
         this.swerveSubsystem = swerveSubsystem;
-        //fieldTags.add(new FieldTag(0, new Pose2d(-1, 0, new Rotation2d(Math.PI))));
-        //fieldTags.add(new FieldTag(1, new Pose2d(1.7, 0.8, new Rotation2d(-Math.PI/2))));
-
-//        Pose2d tag1Pose = new Pose2d(-aprilTagX,centerTagY+distBetweenTags,new Rotation2d(Math.PI));
-//        Pose2d tag2Pose = new Pose2d(-aprilTagX,centerTagY,new Rotation2d(Math.PI));
-//        Pose2d tag3Pose = new Pose2d(-aprilTagX,centerTagY-distBetweenTags,new Rotation2d(Math.PI));
-//
-//        Pose2d tag6Pose = new Pose2d(aprilTagX,centerTagY-distBetweenTags,new Rotation2d());
-//        Pose2d tag7Pose = new Pose2d(aprilTagX,centerTagY,new Rotation2d());
-//        Pose2d tag8Pose = new Pose2d(aprilTagX,centerTagY+distBetweenTags,new Rotation2d());
-//
-//        Pose2d tag4Pose = new Pose2d(-lonesomeAprilTagX, lonesomeAprilTagY, new Rotation2d(Math.PI));
-//        Pose2d tag5Pose = new Pose2d(lonesomeAprilTagX, lonesomeAprilTagY, new Rotation2d());
-//
-//        fieldTags.add(new FieldTag(1,tag1Pose));
-//        fieldTags.add(new FieldTag(2,tag2Pose));
-//        fieldTags.add(new FieldTag(3,tag3Pose));
-//        fieldTags.add(new FieldTag(4,tag4Pose));
-//        fieldTags.add(new FieldTag(5,tag5Pose));
-//        fieldTags.add(new FieldTag(6,tag6Pose));
-//        fieldTags.add(new FieldTag(7,tag7Pose));
-//        fieldTags.add(new FieldTag(8,tag8Pose));
 
 
         for(int i = 0; i<8; i++){
             int id = i+1;
             double x = aprilTagOriginX  -  Units.inchesToMeters(aprilTagYDiffsFromOriginInches[i]);
             double y = aprilTagOriginY - Units.inchesToMeters(aprilTagXDiffsFromOriginInches[i]);
-//            SmartDashboard.putNumber("tagOriginX",aprilTagOriginX);
-//            SmartDashboard.putNumber("tagOriginY",aprilTagOriginY);
-//            SmartDashboard.putNumber("tagDiffX",Units.inchesToMeters(aprilTagXDiffsFromOriginInches[i]));
-//            SmartDashboard.putNumber("tagDiffY",Units.inchesToMeters(aprilTagYDiffsFromOriginInches[i]));
-
-
 
             Rotation2d rotation2d = Rotation2d.fromDegrees(0);
             if(i>=4)
@@ -71,9 +43,6 @@ public class VisionSubsystem extends SubsystemBase {
             fieldTags.add(new FieldTag(id, new Pose2d(x,y,rotation2d)));
         }
 
-//        fieldTags.add(new FieldTag(1,new Pose2d()));
-//        fieldTags.add(new FieldTag(2,new Pose2d(5,0, new Rotation2d())));
-//        fieldTags.add(new FieldTag(3,new Pose2d(0,5, new Rotation2d())));
     pathingTelemetrySub.updateFieldTags(fieldTags);
     }
 
@@ -87,6 +56,7 @@ public class VisionSubsystem extends SubsystemBase {
         for(PhotonTrackedTarget target :camera.getLatestResult().getTargets()){
             int id = target.getFiducialId();
             Transform3d transform = target.getBestCameraToTarget();
+            Transform3d alternateTransform = target.getAlternateCameraToTarget();
             Rotation3d rotation = transform.getRotation();
 
             for(int i = 0; i<fieldTags.size(); i++){
@@ -95,19 +65,12 @@ public class VisionSubsystem extends SubsystemBase {
                 swerveSubsystem.fieldTagSpotted(fieldTags.get(i), transform, camera.getLatestResult().getLatencyMillis(), target.getPoseAmbiguity());
       //          SmartDashboard.putNumber("lastPoseAmbiguity",camera.getLatestResult().getBestTarget().getPoseAmbiguity());
 
+
             }
 
-//      SmartDashboard.putNumber("tag"+id+".X", transform.getX());
-//      SmartDashboard.putNumber("tag"+id+".Y", transform.getY());
-//      SmartDashboard.putNumber("tag"+id+".Z", transform.getZ());
-//
-//      SmartDashboard.putNumber("tag"+id+".rX", rotation.getX());
-//      SmartDashboard.putNumber("tag"+id+".rY", rotation.getY());
-//      SmartDashboard.putNumber("tag"+id+".rZ", rotation.getZ());
 
 
             double rotationFromCamera = Math.IEEEremainder(lastTransform.getRotation().getZ() + Math.PI, 2*Math.PI);
-            // SmartDashboard.putNumber("rotationFromCamera", rotationFromCamera);
 
 
             if(id==0){
@@ -125,6 +88,7 @@ public class VisionSubsystem extends SubsystemBase {
     boolean lastSeenOnRight;
     Transform3d lastTransform = new Transform3d();
     double lastSeenTime;
+    @Deprecated
     public double[] getDesiredSpeeds(){
         double[] out = new double[3];
         double timeSinceSeen = Timer.getFPGATimestamp() - lastSeenTime;
@@ -138,6 +102,8 @@ public class VisionSubsystem extends SubsystemBase {
 
         out[0] = (lastTransform.getX()-1) *0.5; // get one meter from target
         out[1] = lastTransform.getY() * 0.5;
+
+
         // SmartDashboard.putNumber("out0",out[0]);
 
         double rotationFromCamera = Math.IEEEremainder(lastTransform.getRotation().getZ() + Math.PI, 2*Math.PI);
